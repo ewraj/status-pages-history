b893c779d7db14a1364616fc920fabb1856da994
ewraj <rajrxo@gmail.com>
2026-09-22 19:45:02 +0530

    Use clean filenames for scraped status JSON

--- scrape.sh
+++ scrape.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
+
 ./download.sh 'https://status.anthropic.com/api/v2/summary.json'
+mv -f status.anthropic.com-api-v2-summary.json.json anthropic-status.json
+
 ./download.sh 'https://www.githubstatus.com/api/v2/summary.json'
+mv -f githubstatus.com-api-v2-summary.json.json github-status.json
